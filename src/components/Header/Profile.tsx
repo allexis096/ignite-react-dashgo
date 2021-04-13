5fd72eea21d99c3de9b4bd1567d03a1e87fabda2
allexis096 <allexissoares@gmail.com>
2021-04-13 20:51:25 -0300

    :iphone: feat: add responsivity in Header component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export function Profile() {
+type ProfileProps = {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Allexis Figueiredo</Text>
-        <Text color="gray.300" fontSize="small">
-          allex@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Allexis Figueiredo</Text>
+          <Text color="gray.300" fontSize="small">
+            allex@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
