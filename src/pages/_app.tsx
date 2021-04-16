a98c5b4cdc1be47e4fd5fad97c2eca36c9756645
allexis096 <allexissoares@gmail.com>
2021-04-15 21:07:50 -0300

    :sparkles: feat: add miragejs config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../hooks/SidebarDrawerContext'
+import { makeServer } from '../services/mirage'
+
+if (proccess.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
