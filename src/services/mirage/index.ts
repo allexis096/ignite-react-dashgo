365a10e890132d131e47a44067bf6f4170fd916f
allexis096 <allexissoares@gmail.com>
2021-04-17 11:36:48 -0300

    :sparkles: feat: create user complete

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from "miragejs";
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from "miragejs";
 import faker from 'faker';
 
 type User = {
@@ -9,9 +9,14 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+
     models: {
       user: Model.extend<Partial<User>>({}),
     },
+
     factories: {
       user: Factory.extend({
         name(i: number) {
@@ -25,9 +30,11 @@ export function makeServer() {
         }
       })
     },
+
     seeds(server) {
       server.createList('user', 200);
     },
+
     routes() {
       this.namespace = 'api';
       this.timing = 750;
